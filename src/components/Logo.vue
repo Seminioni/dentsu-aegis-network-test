e4077a014e8dbfb288ffb9f691223184bb192d76
Mikhail Semin <seminioni@gmail.com>
2020-09-06 04:21:35 +0800

    chore: rename logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,18 +1,18 @@
 <template>
   <Anchor class="anchor-logo" to="/">
-    <img class="logo" :src="require('@/assets/logo_transparent.png')" alt="Article viewer's logo">
+    <img class="logo" :src="require('@/assets/images/logo.png')" alt="Article viewer's logo">
   </Anchor>
 </template>
 
 <script>
 export default {
-name: "Logo"
-}
+  name: 'Logo',
+};
 </script>
 
 <style scoped>
-  .logo {
-    width: 140px;
-    height: 140px;
-  }
+.logo {
+  width: 140px;
+  height: 140px;
+}
 </style>
